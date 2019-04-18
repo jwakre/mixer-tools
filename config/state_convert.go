ee18a7a1ab875e0d914cce006e573f5b8b415534
John Akre <john.w.akre@intel.com>
2019-04-18 20:12:23 +0000

    config: Add state file version conversion
    Adds support to convert old versions of mixer.state to the latest.
    
    Signed-off-by: John Akre <john.w.akre@intel.com>

---
+++ config/state_convert.go
@@ -0,0 +1,73 @@
+// Copyright © 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/clearlinux/mixer-tools/helpers"
+)
+
+// CurrentStateVersion is the current revision for the state file structure
+const CurrentStateVersion = "1.0"
+
+func (state *MixState) parseVersionAndConvert() error {
+	// Reset version for files without versioning
+	state.version = "0.0"
+
+	f, err := os.Open(state.filename)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	// Read state version
+	reader := bufio.NewReader(f)
+	found, err := state.parseVersion(reader)
+	if err != nil {
+		return err
+	}
+
+	// Already on latest version
+	if found && state.version == CurrentStateVersion {
+		return nil
+	}
+
+	fmt.Printf("Converting state to version %s\n", CurrentStateVersion)
+
+	if err := helpers.CopyFile(state.filename+".bkp", state.filename); err != nil {
+		return err
+	}
+
+	fmt.Printf("Old state saved as %s\n", state.filename+".bkp")
+
+	return state.convertCurrent()
+}
+
+func (state *MixState) convertCurrent() error {
+	// Version only exists in new state, so parse mixer.state as TOML
+	if err := state.parse(); err != nil {
+		return err
+	}
+
+	// Set state to the current version
+	state.version = CurrentStateVersion
+
+	return state.Save()
+}
